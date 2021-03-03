90d178c56c3d0bc0c6fe77f87e0d9d3bfa6eb6dd
Lucas Arcanjo <lucas.saolliveira@gmail.com>
2021-03-03 15:35:16 -0300

    passnado configuraççoes para a nossa modal

--- Tecnicas_avancadas_em_Angular8/curso-angular/src/app/filmes/cadastro-filmes/cadastro-filmes.component.ts
+++ Tecnicas_avancadas_em_Angular8/curso-angular/src/app/filmes/cadastro-filmes/cadastro-filmes.component.ts
@@ -5,6 +5,7 @@ import { MatDialog } from '@angular/material/dialog';
 import { FilmesService } from 'src/app/core/filmes.service';
 import { AlertaComponent } from 'src/app/shared/components/alerta/alerta.component';
 import { ValidarCamposService } from 'src/app/shared/components/validar-campos.service';
+import { Alerta } from 'src/app/shared/models/alerta';
 import { Filme } from 'src/app/shared/models/filme';
 
 @Component({
@@ -57,7 +58,15 @@ export class CadastroFilmesComponent implements OnInit {
 
   private salvar(filme: Filme): void {
     this.filmeService.salvar(filme).subscribe(() => {
-      const dialogRef = this.dialog.open(AlertaComponent);
+      const config = {
+        data: {
+          btnSucesso: 'Ir para a listagem',
+          btnCancelar: 'Cadastrar um novo filme',
+          corBtnCancelar: 'primary',
+          possuirBtnFechar: true
+        } as Alerta
+      };
+      const dialogRef = this.dialog.open(AlertaComponent, config);
     }, () => {
       alert('ERRO AO SALVAR');
     });
