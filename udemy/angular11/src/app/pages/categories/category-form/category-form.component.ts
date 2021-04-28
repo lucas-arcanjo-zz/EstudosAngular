7c20a79d9ccf54e5001fda4c88f026e45573aa06
Lucas Arcanjo <lucas.saolliveira@gmail.com>
2021-04-28 11:50:09 -0300

    Implementando o template do componente 'CategoryForm'

--- udemy/angular11/src/app/pages/categories/category-form/category-form.component.ts
+++ udemy/angular11/src/app/pages/categories/category-form/category-form.component.ts
@@ -16,60 +16,60 @@ import { toastr } from "toastr";
 })
 export class CategoryFormComponent implements OnInit, AfterContentChecked {
 
-  currentAction: string;
-  categoryForm: FormGroup;
-  pageTitle: string;
-  serveErrorMessages: String[] = null;
-  submittingForm: boolean = false;
-  category: Category = new Category();
+  // currentAction: string;
+  // categoryForm: FormGroup;
+  // pageTitle: string;
+  // serveErrorMessages: String[] = null;
+  // submittingForm: boolean = false;
+  // category: Category = new Category();
 
   constructor(
-    private categoryService: CategoryService,
-    private route: ActivatedRoute,
-    private router: Router,
-    private formBuilder: FormBuilder
+    // private categoryService: CategoryService,
+    // private route: ActivatedRoute,
+    // private router: Router,
+    // private formBuilder: FormBuilder
   ) { }
 
   ngOnInit() {
-    this.setCurrentAction();
-    this.buildCategoryForm();
-    this.loadCategory();
+    // this.setCurrentAction();
+    // this.buildCategoryForm();
+    // this.loadCategory();
   }
 
   ngAfterContentChecked() {
   }
 
-  // PRIVATE METHODS
-  private setCurrentAction() {
-    if (this.route.snapshot.url[0].path == "new")
-      this.currentAction = "new"
-    else
-      this.currentAction = "edit"
-  }
+  // // PRIVATE METHODS
+  // private setCurrentAction() {
+  //   if (this.route.snapshot.url[0].path == "new")
+  //     this.currentAction = "new"
+  //   else
+  //     this.currentAction = "edit"
+  // }
 
-  private buildCategoryForm() {
-    this.categoryForm = this.formBuilder.group({
-      id: [null],
-      name: [null, [Validators.required, Validators.minLength(2)]],
-      description: [null]
-    });
-  }
+  // private buildCategoryForm() {
+  //   this.categoryForm = this.formBuilder.group({
+  //     id: [null],
+  //     name: [null, [Validators.required, Validators.minLength(2)]],
+  //     description: [null]
+  //   });
+  // }
 
-  private loadCategory() {
-    if (this.currentAction == "edit") {
+  // private loadCategory() {
+  //   if (this.currentAction == "edit") {
 
-      this.route.paramMap.pipe(
-        switchMap(params => this.categoryService.getById(+params.get("id")))
-      )
-        .subscribe(
-          (category) => {
-            this.category = category;
-            this.categoryForm.patchValue(category) // binds loaded category data to CategoryForm
-          },
-          (error) => alert('Ocorreu um erro no servidor, tente mais tarde.')
-        )
-    }
-  }
+  //     this.route.paramMap.pipe(
+  //       switchMap(params => this.categoryService.getById(+params.get("id")))
+  //     )
+  //       .subscribe(
+  //         (category) => {
+  //           this.category = category;
+  //           this.categoryForm.patchValue(category) // binds loaded category data to CategoryForm
+  //         },
+  //         (error) => alert('Ocorreu um erro no servidor, tente mais tarde.')
+  //       )
+  //   }
+  // }
 
 
 }
