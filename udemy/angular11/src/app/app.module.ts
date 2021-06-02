041a0926d5bd14ad6795e9b451491bd4b55074ab
Lucas Arcanjo <lucas.saolliveira@gmail.com>
2021-06-02 13:10:50 -0300

    config

--- udemy/angular11/src/app/app.module.ts
+++ udemy/angular11/src/app/app.module.ts
@@ -5,8 +5,6 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { HttpClientInMemoryWebApiModule } from "angular-in-memory-web-api";
-import { InMemoryDatabase } from "./in-memory-database";
 
 @NgModule({
   declarations: [
@@ -16,7 +14,6 @@ import { InMemoryDatabase } from "./in-memory-database";
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    HttpClientInMemoryWebApiModule.forRoot(InMemoryDatabase)
   ],
   providers: [],
   bootstrap: [AppComponent]
