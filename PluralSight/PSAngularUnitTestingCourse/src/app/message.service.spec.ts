66c9edb8488ab97493d4181d71f1351eccfe0bc1
Lucas Arcanjo <lucas.saolliveira@gmail.com>
2021-06-24 12:08:44 -0300

    improving service spec

--- PluralSight/PSAngularUnitTestingCourse/src/app/message.service.spec.ts
+++ PluralSight/PSAngularUnitTestingCourse/src/app/message.service.spec.ts
@@ -6,7 +6,7 @@ describe('MessageService', () => {
     it('should have no message to start', () => {
         service = new MessageService();
         
-        expect(service.messages.length).toEqual(0);
+        expect(service.messages.length).toBe(0);
     })
 
     it('should add a message when add is called', () => {
@@ -14,15 +14,15 @@ describe('MessageService', () => {
 
         service.add('testing');
 
-        expect(service.messages.length).toEqual(1);
+        expect(service.messages.length).toBe(1);
     })
 
     it('should remove all messages when clear is called', () => {
         service = new MessageService();
         service.add('testing1');
-        
+
         service.clear();
 
-        expect(service.messages.length).toEqual(0)
+        expect(service.messages.length).toBe(0)
     })
 })
