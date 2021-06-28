b875e675323a3b5df6d3d771e941323bb5df5157
Lucas Arcanjo <lucas.saolliveira@gmail.com>
2021-06-28 16:52:20 -0300

    creating a deep integration test

--- PluralSight/PSAngularUnitTestingCourse/src/app/heroes/heroes.component.deep.spec.ts
+++ PluralSight/PSAngularUnitTestingCourse/src/app/heroes/heroes.component.deep.spec.ts
@@ -4,20 +4,13 @@ import { By } from "@angular/platform-browser";
 import { of } from "rxjs";
 import { Hero } from "../hero";
 import { HeroService } from "../hero.service";
+import { HeroComponent } from "../hero/hero.component";
 import { HeroesComponent } from "./heroes.component"
 
 describe('HeroesComponent (shallow tests)', () => {
     let fixture: ComponentFixture<HeroesComponent>;
     let mockHeroService;
     let HEROES;
-
-    @Component({
-        selector: 'app-hero',
-        template: '<div></div>'
-    })
-    class FakeHeroComponent {
-        @Input() hero: Hero;
-    }
 
     beforeEach(() => {
         HEROES = [
@@ -29,27 +22,20 @@ describe('HeroesComponent (shallow tests)', () => {
         TestBed.configureTestingModule({
             declarations: [
                 HeroesComponent,
-                FakeHeroComponent
+                HeroComponent
             ],
             providers: [
                 { provide: HeroService, useValue: mockHeroService }
             ],
-            // schemas: [NO_ERRORS_SCHEMA]
+            schemas: [NO_ERRORS_SCHEMA]
         })
         fixture = TestBed.createComponent(HeroesComponent);
-    })
-
-    it('should set heroes correctly from the service', () => {
         mockHeroService.getHeroes.and.returnValue(of(HEROES));
+        
         fixture.detectChanges();
-
-        expect(fixture.componentInstance.heroes.length).toBe(3)
     })
 
-    it('should create one li for each hero', () => {
-        mockHeroService.getHeroes.and.returnValue(of(HEROES));
-        fixture.detectChanges();
-
-        expect(fixture.debugElement.queryAll(By.css('li')).length).toBe(3);
+    it('should be true', () => {
+        expect(true).toBe(true);
     })
-})
+});
